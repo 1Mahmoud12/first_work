f646bf475d4926b29c94edcd2b1a306f6ea116b0
mahmoud medhat <mahmoudmdhat852@gmail.com>
2022-07-26 08:13:31 +0200

    main_to_do

---
+++ lib/module/to_do_layout.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class ToDoLayout extends StatelessWidget {
+  const ToDoLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Work'),),
+    );
+  }
+}
